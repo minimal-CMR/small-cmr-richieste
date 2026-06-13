03eae3759bfef9b73fb0cf0be2ba66e7fc817adb
Dose99 <dosepaolo@gmail.com>
2026-06-13 19:01:10 +0200

    fix: refactor Approvazioni to SFC pattern + axios interceptor for auth

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,19 +2,10 @@ import axios from 'axios';
 
 const api = axios.create({ baseURL: '' });
 
-const token = localStorage.getItem('token');
-if (token) {
-  api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-}
-
-window.addEventListener('storage', e => {
-  if (e.key === 'token') {
-    if (e.newValue) {
-      api.defaults.headers.common['Authorization'] = `Bearer ${e.newValue}`;
-    } else {
-      delete api.defaults.headers.common['Authorization'];
-    }
-  }
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
 
 export default api;
